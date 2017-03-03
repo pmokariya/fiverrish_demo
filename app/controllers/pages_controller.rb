11ed49eafb14db0cf6c758cad4bbaf1dd1c9b548
pooja mokariya <poojalm747@gmail.com>
2017-03-03 17:52:04 +0530

    Changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
   	if current_user.present?
-  		@services = current_user.services.order(created_at: :desc).limit(2)
+  		@services = current_user.services.order(created_at: :desc).limit(4)
   	else
-  		@services = Service.all.order(created_at: :desc).limit(2)
+  		@services = Service.all.order(created_at: :desc).limit(4)
   	end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user = User.create(fullname:"Pooja Mokariya",email:"poojalm747@gmail.com",password:"12345678",password_confirmation:"12345678")
+ary = [1,2,3,4,5]
+ary.each do |i|
+	@service = @user.services.new(title:"#{i}I will write 300 word blog posts",description:"I will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog postsI will write 300 word blog posts",price:"500",requirements:"1")
+	@service.save
+end
+
