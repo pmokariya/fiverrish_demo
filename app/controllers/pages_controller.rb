00b3930ceac69e20ed2b251b8151a4dfbb91fbfe
pooja mokariya <poojalm747@gmail.com>
2017-03-03 17:16:37 +0530

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+  	if current_user.present?
+  		@services = current_user.services.order(created_at: :desc).limit(2)
+  	else
+  		@services = Service.all.order(created_at: :desc).limit(2)
+  	end
   end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,7 +2,7 @@ class ServicesController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   before_action :set_service, only: [:show, :edit, :update]
   def index
-    @services = current_user.services
+    @services = current_user.services if current_user.services.present?
   end
 
   def show
@@ -14,6 +14,17 @@ class ServicesController < ApplicationController
 
   def create
     # TODO: Save the newly created service. Redirect to an appropriate page if save fails.
+    @user = current_user
+    @service = @user.services.new(service_params)
+    respond_to do |format|
+      if @service.save
+        format.html { redirect_to @service, notice: 'Service was successfully created.' }
+        format.json { render :show, status: :created, location: @service }
+      else
+        format.html { render :new }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -21,10 +32,30 @@ class ServicesController < ApplicationController
 
   def update
     # TODO: Save the updated service. Redirect to an appropriate page if save fails.
+    respond_to do |format|
+      if @service.update(service_params)
+        format.html { redirect_to services_path, notice: 'Service was successfully updated.' }
+        format.json { render :show, status: :ok, location: @service }
+      else
+        format.html { render :edit }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @service.destroy
+    respond_to do |format|
+      format.html { redirect_to services_path, notice: 'Service was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
   def set_service
     @service = Service.find(params[:id])
+  end
+  def service_params
+    params.require(:service).permit!
   end
 end
